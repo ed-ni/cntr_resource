2266a6186e09a8c185a767e279427e500a048f8c
ed <ed.nikolaev@gmail.com>
2021-03-05 16:33:56 +0300

    c2

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,28 @@
 
 #include <AceButton.h>
 
-#define buzz 2
-#define butt1 PB2  
-uint8_t ledPin = butt1;
+#define buzz PB3
+#define ledPin PB2  
+#define buttInner_MISO PB1
+#define buttOuter_MOSI PB0  
+#define swCoil PB4  
+//uint8_t ledPin = butt1;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(PB2, OUTPUT);
-  }
+  pinMode(buzz, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(swCoil, OUTPUT);
+  pinMode(buttInner_MISO, INPUT_PULLUP);
+  pinMode(buttOuter_MOSI, INPUT_PULLUP);
+ }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+   digitalWrite(buzz, HIGH);   // turn the LED on (HIGH is the voltage level)
+ delay(500);                       // wait for a second
   digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(buzz, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
